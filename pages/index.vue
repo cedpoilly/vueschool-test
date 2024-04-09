333ec54351c08bc4582b1eb3d975741c1561dda1
cedpoilly <cedpoilly@gmail.com>
2024-04-09 14:25:27 +0400

    style: DaisyUI + Rubik font

--- pages/index.vue
+++ pages/index.vue
@@ -13,3 +13,19 @@ useHead({
   <WorkShops />
   <TheFooter />
 </template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap");
+
+.rubik-font {
+  font-family: "Rubik", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 400;
+  font-style: normal;
+}
+
+body {
+  font-family: "Rubik", sans-serif;
+  font-size: 16px;
+}
+</style>
