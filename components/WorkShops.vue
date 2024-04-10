7ee14b26c66c5d9ab92806d8763335cb331399dc
cedpoilly <cedpoilly@gmail.com>
2024-04-10 19:59:49 +0400

    feat: [mobile] Workshops

--- components/WorkShops.vue
+++ components/WorkShops.vue
@@ -1,5 +1,128 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const workshops = ref([
+  {
+    id: 1,
+
+    header: {
+      text: "December 14th - 11:00 AM (GMT-3)",
+      iconURL: "images/workshop-calendar-icon.svg",
+    },
+
+    courseInfo: {
+      type: {
+        label: "LIVE Training",
+        icon: "images/workshop-type-live-icon.svg",
+      },
+      duration: {
+        label: "15 Hours",
+        icon: "images/workshop-duration-icon.svg",
+      },
+    },
+
+    courseTitle: "Vue.js Fundamentals",
+    courseDescription:
+      "A progressive framework for building user interfaces. Unlike other monolithic frameworks, Vue is designed from the ground up to be incrementally...",
+
+    author: {
+      name: "Steve Popoola",
+      avatarURL: "images/steve-popoola-avatar.png",
+    },
+  },
+
+  {
+    id: 2,
+
+    header: {
+      text: "December 14th - 11:00 AM (GMT-3)",
+      iconURL: "images/workshop-calendar-icon.svg",
+    },
+
+    courseInfo: {
+      type: {
+        label: "LIVE Training",
+        icon: "images/workshop-type-live-icon.svg",
+      },
+      duration: {
+        label: "15 Hours",
+        icon: "images/workshop-duration-icon.svg",
+      },
+    },
+
+    courseTitle: "Vue 3 and Composition API",
+    courseDescription:
+      "Take a deep dive into the new and exciting features of Vue 3. Learn about Portals, Suspense, Composition API, Fragments, and much much more...",
+
+    author: {
+      name: "Steve Popoola",
+      avatarURL: "images/steve-popoola-avatar.png",
+    },
+  },
+  {
+    id: 3,
+
+    header: {
+      text: "December 14th - 11:00 AM (GMT-3)",
+      iconURL: "images/workshop-calendar-icon.svg",
+    },
+
+    courseInfo: {
+      type: {
+        label: "LIVE Training",
+        icon: "images/workshop-type-live-icon.svg",
+      },
+      duration: {
+        label: "15 Hours",
+        icon: "images/workshop-duration-icon.svg",
+      },
+    },
+
+    courseTitle: "Vue.js Fundamentals",
+    courseDescription:
+      "A progressive framework for building user interfaces. Unlike other monolithic frameworks, Vue is designed from the ground up to be incrementally...",
+
+    author: {
+      name: "Steve Popoola",
+      avatarURL: "images/steve-popoola-avatar.png",
+    },
+  },
+])
+</script>
 
 <template>
-  <section>Workshopssss!</section>
+  <section>
+    <article>
+      <header class="text-center">
+        <h2 class="font-bold text-[40px] text-white leading-[47.4px] mb-14">
+          Available Workshops
+        </h2>
+      </header>
+
+      <main class="mb-10">
+        <div
+          class="carousel carousel-center max-w-[calc(100vw-2rem)] space-x-4 rounded-box"
+        >
+          <div
+            v-for="workshopItem in workshops"
+            :key="workshopItem.id"
+            :id="'item' + workshopItem.id"
+            class="carousel-item"
+          >
+            <WorkshopItem :workshop="workshopItem" />
+          </div>
+        </div>
+
+        <div class="flex justify-center w-full py-2 gap-2">
+          <a href="#item1" class="btn btn-xs">1</a>
+          <a href="#item2" class="btn btn-xs">2</a>
+          <a href="#item3" class="btn btn-xs">3</a>
+        </div>
+      </main>
+
+      <footer class="grid justify-center">
+        <button class="btn btn-primary text-[#212435] py-5 px-10 h-14">
+          See all the workshops
+        </button>
+      </footer>
+    </article>
+  </section>
 </template>
