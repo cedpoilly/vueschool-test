abbb2db553f87b8e2fda629c4f2d902cfc93f6ee
cedpoilly <cedpoilly@gmail.com>
2024-04-11 14:19:39 +0400

    style: desktop hero section

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -11,14 +11,20 @@ const buttonLabel = "Talk to sales"
 </script>
 
 <template>
-  <section class="grid gap-y-12 justify-center">
-    <div class="text-center grid gap-y-6 justify-center md:max-w-xl">
+  <section
+    class="grid gap-y-12 justify-center lg:grid-cols-[auto_auto] lg:items-center lg:justify-center lg:gap-x-[52px]"
+  >
+    <div
+      class="text-center grid gap-y-6 justify-center md:max-w-xl lg:max-w-xl lg:gap-y-10 lg:text-left lg:justify-center"
+    >
       <h1 class="text-[40px] leading-[47.4px] md:text-6xl font-bold">
         {{ title.preEm }} <em class="em">{{ title.em }}</em> {{ title.postEm }}
       </h1>
-      <p class="md:text-[22px] md:leading-[30px] md:mb-10">{{ description }}</p>
+      <p class="md:text-[22px] md:leading-[30px] md:mb-10 lg:mb-0">
+        {{ description }}
+      </p>
       <button
-        class="btn btn-primary w-fit mx-auto px-10 py-5 h-auto md:text-base"
+        class="btn btn-primary w-fit mx-auto px-10 py-5 h-fit md:text-base lg:mx-0"
       >
         {{ buttonLabel }}
       </button>
