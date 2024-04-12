ad2fb8cee9fab9adb362de553fa79b8571a1d596
cedpoilly <cedpoilly@gmail.com>
2024-04-12 22:22:19 +0400

    chore: configure @nuxtjs/sanity

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -4,12 +4,26 @@ export default defineNuxtConfig({
     componentIslands: true,
   },
 
+  modules: ["@nuxtjs/sanity"],
+
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
+    },
+  },
+
+  sanity: {
+    projectId: process.env.NUXT_SANITY_PROJECT_ID,
+    dataset: process.env.NUXT_SANITY_DATASET,
+    useCdn: true,
+    apiVersion: process.env.NUXT_SANITY_API_VERSION || "2024-03-15",
+    visualEditing: {
+      studioUrl: process.env.NUXT_SANITY_STUDIO_URL || "http://localhost:3333",
+      token: process.env.NUXT_SANITY_API_READ_TOKEN,
+      stega: true,
     },
   },
 
